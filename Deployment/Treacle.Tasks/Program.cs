7fb4199c00ce8122cabaee799df683c8d630e84a
Duncanr <duncan.butler1@btinternet.com>
2012-04-06 19:21:44 +0100

    initial commit

---
+++ Deployment/Treacle.Tasks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treacle.Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
